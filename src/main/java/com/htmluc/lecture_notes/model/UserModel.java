4ca138ea5ba036c4c3c95e22a4c8974e40ae65b3
Lucas Dantas <lucasmedeiros@trt21.jus.br>
2026-01-27 10:02:15 -0300

    feat: create user table

---
+++ src/main/java/com/htmluc/lecture_notes/model/UserModel.java
@@ -0,0 +1,26 @@
+package com.htmluc.lecture_notes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "tb_users") // Anotação define uma entidade
+public class UserModel {
+
+    @Id // Anotação que define a chame primária
+    @GeneratedValue(generator = "UUID") // Anotação que gera valores
+    private UUID id;
+
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
