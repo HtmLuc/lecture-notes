e06ce19b99d0188c08a483aaa91b0c77035dc97a
Lucas Dantas <lucasmedeiros@trt21.jus.br>
2026-01-27 08:37:16 -0300

    build: add lombok dependency

--- src/main/java/com/htmluc/lecture_notes/model/NewsModel.java
+++ src/main/java/com/htmluc/lecture_notes/model/NewsModel.java
@@ -1,31 +1,13 @@
 package com.htmluc.lecture_notes.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class NewsModel {
     long id;
     String title;
     String category;
     String author;
-
-    public NewsModel(long id, String title, String category, String author) {
-        this.id = id;
-        this.title = title;
-        this.category = category;
-        this.author = author;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
 }
