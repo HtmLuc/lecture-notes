c94070902a58ce7af57d325d7433d455247dfef6
Lucas Dantas <lucasmedeiros@trt21.jus.br>
2026-01-28 07:50:39 -0300

    feat: add hash password

--- src/main/java/com/htmluc/lecture_notes/controller/UserController.java
+++ src/main/java/com/htmluc/lecture_notes/controller/UserController.java
@@ -1,9 +1,9 @@
 package com.htmluc.lecture_notes.controller;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.htmluc.lecture_notes.model.UserModel;
 import com.htmluc.lecture_notes.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +24,10 @@ public class UserController {
         if (user != null) {
             return ResponseEntity.badRequest().build();
         }
+
+        var passwordHashered = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashered);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.ok(userCreated);
